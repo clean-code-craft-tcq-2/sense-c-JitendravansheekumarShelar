d1e9019e8b302aabfbe9d7fa570ae8c19ee00a96
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2021-10-22 00:23:44 +0530

    Update stats.h
    declaring functions as extern

--- stats.h
+++ stats.h
@@ -13,3 +13,5 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
+extern void emailAlerter(void);
+extern void ledAlerter(void);
