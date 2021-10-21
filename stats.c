bf9f0c98ecbaef0c4959ff315e4d2bbf9fdb2143
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2021-10-21 23:53:45 +0530

    Update stats.c
    Algorithm is added for min, max & average

--- stats.c
+++ stats.c
@@ -2,9 +2,25 @@
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    
+    if (numberset != NULL)
+    {
+        s.average = s.min = s.max = numberset[0];    
+        for (i = 1; i < setlength ; i++)
+        {
+            if (s.min > numberset[i])
+                s.min = numberset[i];
+            if (s.max < numberset[i])
+                s.max = numberset[i];
+        
+            s.average = s.average + numberset[i];
+        }    
+        s.average = s.average / (float)setlength ;
+        return s;
+    }
+    s.average = s.min = s.max = NAN;
+    return s;
+
 }
 
 int emailAlertCallCount = 0;
